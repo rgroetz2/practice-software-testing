df33d0250f3a5a21ed974538965262389e44ab45
rgroetz2 <96633945+rgroetz2@users.noreply.github.com>
2024-02-18 19:31:25 +0100

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -1,1 +1,3 @@
 docker-compose up -d
+#hier haben wir eine Aenderung und commit durchgefuehrt nachdem branch BF1 und BF2 gemacht wurden
+#BF1 wird diese Datei auch ändern, ohne aber vorher zu pullen. Beim commit von BF1 muss es dann zu einem merge conflict kommen.
