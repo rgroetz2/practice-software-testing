24507f0d03f43c2ba82752f1fae9a9e61a20b1c1
rgroetz2 <96633945+rgroetz2@users.noreply.github.com>
2024-03-01 16:15:54 +0100

    Update playwright.config.ts
    baseurl set to IP

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4200',
+    /* baseURL: 'http://localhost:4200', */
+    baseURL: 'http://109.176.250.87:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
