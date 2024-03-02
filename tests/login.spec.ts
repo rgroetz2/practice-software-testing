f2ae3f4fc5d7825ee505bf040ba50defd46843ea
rgroetz2 <96633945+rgroetz2@users.noreply.github.com>
2024-03-02 13:39:00 +0100

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,7 @@ test.describe('Login Feature', () => {
     test('Login_withValidUserCredentials_dasboardIsDisplayed', async ({page}) => {
         await page.goto('http://109.176.250.87:4200/#/auth/login');
 
-        await page.screenshot({ path: './playwright-report/screenshot.png' });
+        await page.screenshot({ path: 'playwright-report/screenshot.png' });
 
         await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
         await page.locator('[data-test="password"]').fill('welcome01');
