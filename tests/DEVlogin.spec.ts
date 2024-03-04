9c1523c847b965023cb154e3d49736ed5ff6132f
rgroetz2 <96633945+rgroetz2@users.noreply.github.com>
2024-03-04 19:28:00 +0100

    Update and rename login.spec.ts to DEVlogin.spec.ts

--- tests/DEVlogin.spec.ts
+++ tests/DEVlogin.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 
 test.describe('Login Feature', () => {
 
-    test('Login_withValidUserCredentials_dasboardIsDisplayed', async ({page}) => {
+    test('DLogin_withValidUserCredentials_dasboardIsDisplayed', async ({page}) => {
         await page.goto('http://109.176.250.87:4200/#/auth/login');
 
         await page.screenshot({ path: 'playwright-report/screenshot.png' });
